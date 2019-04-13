a327557235948c59709a3412598eff80bf8c144a
AntonGrn <45007883+AntonGrn@users.noreply.github.com>
2019-04-13 18:06:32 +0200

    Abstract generic class Gadget with concrete sub classes

---
+++ ClientApplication/src/mainPackage/Heat.java
@@ -0,0 +1,14 @@
+package mainPackage;
+
+public class Heat extends Gadget<Integer> {
+
+    public Heat(String name, int state, int consumption, String room) {
+        super(name, state, consumption, room);
+    }
+
+    public Heat(String name, int consumption, String room) {
+        super(name, consumption, room);
+        state = 0;
+    }
+
+}
