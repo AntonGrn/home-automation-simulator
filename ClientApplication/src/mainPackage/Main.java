fb9d47bf679518b81e7c5284a300ea6464d77f6a
AntonGrn <45007883+AntonGrn@users.noreply.github.com>
2019-04-18 10:24:54 +0200

    Instantiated MainWindoeController as Singleton object

--- ClientApplication/src/mainPackage/Main.java
+++ ClientApplication/src/mainPackage/Main.java
@@ -8,11 +8,22 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static MainWindowController mainWindowController;  //We set this as instance, so we can reach it later on.
+
+    public static MainWindowController getMainWindowController(){  //Our way of accessing our one-and-only Controller object
+        return mainWindowController;
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        primaryStage.setTitle("LAAS");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        Parent root = (Parent)loader.load();
+        mainWindowController = (MainWindowController) loader.getController();
+
         primaryStage.setScene(new Scene(root));
+        // For setting icon: primaryStage.getIcons().add(new Image("IMAGE URL"));
+        primaryStage.setTitle("LAAS");
+
         primaryStage.show();
     }
 
