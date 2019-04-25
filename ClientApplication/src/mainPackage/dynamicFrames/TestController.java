c9268e02bfb338fd65609aa777cd8e4442482d51
AntonGrn <45007883+AntonGrn@users.noreply.github.com>
2019-04-25 08:02:33 +0200

    Further socket implementation at client end

--- ClientApplication/src/mainPackage/dynamicFrames/TestController.java
+++ ClientApplication/src/mainPackage/dynamicFrames/TestController.java
@@ -1,7 +1,9 @@
 package mainPackage.dynamicFrames;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import mainPackage.Main;
+import mainPackage.ServerConnection;
 import mainPackage.modelClasses.Account;
 import mainPackage.AccountLoggedin;
 import mainPackage.DynamicFrame;
@@ -16,6 +18,22 @@ public class TestController implements DynamicFrame {
         //Simulate a login:
         Account a1 = new Account("Anton");
         AccountLoggedin.getInstance().setLoggedInAccount(a1);
+    }
+
+    @FXML
+    public void login(ActionEvent event) {
+        ServerConnection client = new ServerConnection();
+        try {
+            client.connectToServer();
+            String name = "Anton";
+            String password = "123";
+            String serverRequest = String.format("%s%s%s%s","1:", name, ":", password);
+            Main.getMainWindowController().requestsToServer.put(serverRequest);
+        } catch (Exception e) {
+            System.out.println("Unable to connect to server");
+            System.out.println(e.getMessage());
+        }
+
     }
 
     public void updateFrame() {
