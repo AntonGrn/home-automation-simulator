55e2e34a1cf94ea9bbbb82dfae4df64dce8073c4
Ludvig <ludvigjonsson28@gmail.com>
2019-05-14 23:48:04 +0200

    did some changes in fxml gadget file, and added more featrues and methods into gadgetController

--- ClientApplication/src/mainPackage/dynamicFrames/GadgetsController.java
+++ ClientApplication/src/mainPackage/dynamicFrames/GadgetsController.java
@@ -7,9 +7,6 @@ import mainPackage.DynamicFrame;
 import mainPackage.Main;
 import mainPackage.modelClasses.Gadget;
 import mainPackage.modelClasses.GadgetTableItem;
-import mainPackage.modelClasses.Lamp;
-
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class GadgetsController implements DynamicFrame {
@@ -80,7 +77,7 @@ public class GadgetsController implements DynamicFrame {
         clmRoom.setCellValueFactory(new PropertyValueFactory<>("room"));
         clmType.setCellValueFactory(new PropertyValueFactory<>("type"));
 
-        //This will be using a list that has itterated through the gadgetList in MainwindowController
+        //This will be using a list that has looped through the gadgetList in MainwindowController
         tblViewCurrentGadgets.getItems().addAll(gadgetTableItemsList);
 
     }
@@ -111,6 +108,7 @@ public class GadgetsController implements DynamicFrame {
 
                     //Creating Laas string according to protocol.
                     String serverReguest = String.format("%s%s%s%s%s%s%s%s%s%s%s%s%s%s", "5:", type, ":", id, ":", keepOrRemove, ":", name, ":", room, ":", state, ":", consumption);
+                    System.out.println(serverReguest);
 
                             /*try {
                                 Main.getMainWindowController().requestsToServer.put(serverReguest);
@@ -132,6 +130,10 @@ public class GadgetsController implements DynamicFrame {
         //Sets user data according to action requested
         btnSubmit.setUserData("addGadget");
 
+        //clearing textfields
+        txtfldGadgetName.clear();
+        txtfldConsumption.clear();
+
         //enabling txtfields and buttons and dropdownmenus
         txtfldConsumption.setDisable(false);
         txtfldGadgetName.setDisable(false);
@@ -141,66 +143,109 @@ public class GadgetsController implements DynamicFrame {
     }
 
     public void onEditGadgetClicked() {
-        //Sets user data according to action requested
-        btnSubmit.setUserData("editGadget");
+        if (tblViewCurrentGadgets.getSelectionModel().getSelectedItem()!=null){
+            //Sets user data according to action requested
+            btnSubmit.setUserData("editGadget");
 
-        //enabling txtfields and buttons and dropdownmenus
-        txtfldConsumption.setDisable(false);
-        txtfldGadgetName.setDisable(false);
-        dropMenuGadgets.setDisable(false);
-        dropMenuRooms.setDisable(false);
-        btnSubmit.setDisable(false);
+            //clearing textfields
+            txtfldGadgetName.clear();
+            txtfldConsumption.clear();
+
+
+            GadgetTableItem gadgetTableItem = tblViewCurrentGadgets.getSelectionModel().getSelectedItem();
+            for (Gadget g: Main.getMainWindowController().gadgetList){
+                if (g.getId() == gadgetTableItem.getId()){
+                    //adding info from tableview into textfields and dropDownMenus
+                    txtfldConsumption.setText(String.valueOf(g.getConsumption()));
+                    txtfldGadgetName.setText(g.getName());
+                    dropMenuRooms.getSelectionModel().select(g.getRoom());
+                    dropMenuGadgets.getSelectionModel().select(g.getClass().getSimpleName());
+                    break;
+                }
+            }
+
+            //enabling txtfields and buttons and dropdownmenus
+            txtfldConsumption.setDisable(false);
+            txtfldGadgetName.setDisable(false);
+            dropMenuGadgets.setDisable(false);
+            dropMenuRooms.setDisable(false);
+            btnSubmit.setDisable(false);
+        }else {
+            Main.getMainWindowController().exceptionLabel.setText("Select a row with valid information in it..");
+        }
+
     }
 
     public void onSubmitClicked() {
         //Actions depends on the userData
         switch (btnSubmit.getUserData().toString()) {
             case "editGadget":
-                //Makes sure selected row is not empty
-                if (tblViewCurrentGadgets.getSelectionModel().getSelectedItem() != null) {
-                    GadgetTableItem gadgetItem = tblViewCurrentGadgets.getSelectionModel().getSelectedItem();
+                GadgetTableItem gadgetItem = tblViewCurrentGadgets.getSelectionModel().getSelectedItem();
 
-                    //Loops through gadgetList in main too find the correct gadget (compares gadget id)
-                    for (Gadget g : Main.getMainWindowController().gadgetList) {
-                        if (g.getId() == gadgetItem.getId()) {
-                            //Make request to server for update of gadget (either change of consumption or change of name)
+                String regex = "\\d+"; //0-9 one or more times
+                //Makes sure the textfields not are empty
+                if (!txtfldConsumption.getText().trim().isEmpty() &&
+                        !txtfldGadgetName.getText().trim().isEmpty()) {
 
-                            String type = g.getClass().getSimpleName();
-                            String newName = txtfldGadgetName.getText();
-                            String newConsumption = txtfldConsumption.getText();
-                            String id = String.valueOf(g.getId());
-                            String keepOrRemove = "keep";
-                            String state = String.valueOf(g.getState());
-                            String room = g.getRoom();
+                    //makes sure the input in consumption is correct..
+                    if (txtfldConsumption.getText().matches(regex)) {
 
-                            //Creating Laas string according to protocol.
-                            String serverRequest = String.format("%s%s%s%s%s%s%s%s%s%s%s%s%s%s", "5:", type, ":", id, ":", keepOrRemove, ":", newName, ":", room, ":", state, ":", newConsumption);
+                        //Loops through gadgetList in main too find the correct gadget (compares gadget id)
+                        for (Gadget g : Main.getMainWindowController().gadgetList) {
+                            if (g.getId() == gadgetItem.getId()) {
+                                //Make request to server for update of gadget (either change of consumption or change of name)
 
+                                String type = g.getClass().getSimpleName();
+                                String newName = txtfldGadgetName.getText();
+                                String newConsumption = txtfldConsumption.getText();
+                                String id = String.valueOf(g.getId());
+                                String keepOrRemove = "keep";
+                                String state = String.valueOf(g.getState());
+                                String room = g.getRoom();
+
+                                //Creating Laas string according to protocol.
+                                String serverRequest = String.format("%s%s%s%s%s%s%s%s%s%s%s%s%s%s", "5:", type, ":", id, ":", keepOrRemove, ":", newName, ":", room, ":", state, ":", newConsumption);
+                                System.out.println(serverRequest);
                             /*try {
                                 //putting serverRequest into requestToServer list.
                                 Main.getMainWindowController().requestsToServer.put(serverRequest);
+                                */
+
+                                //setting all buttons,textfields and dropmenus as disabled so user have to click for example "add gadget"
+                                //after the request has been sent and none exceptions has been caught
+                                txtfldConsumption.setDisable(true);
+                                txtfldGadgetName.setDisable(true);
+                                dropMenuGadgets.setDisable(true);
+                                dropMenuRooms.setDisable(true);
+                                btnSubmit.setDisable(true);
+
+                            /*
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             } catch (Exception e) {
                                 Main.getMainWindowController().exceptionLabel.setText("Could not update gadget info..");
                             }*/
+                            }
                         }
+                    } else {
+                        Main.getMainWindowController().exceptionLabel.setText("Please enter a valid consumption number..");
                     }
                 } else {
-                    Main.getMainWindowController().exceptionLabel.setText("Please select a valid row in table..");
+                    Main.getMainWindowController().exceptionLabel.setText("Please enter valid information in both fields..");
                 }
                 break;
+
             case "addGadget":
                 //Makes sure so a room and gadget is selected for insertion to server
                 if (dropMenuGadgets.getSelectionModel().getSelectedItem() != null ||
                         dropMenuRooms.getSelectionModel().getSelectedItem() != null) {
 
-                    String regex = "\\d+"; //0-9 on or more times
+                    String reg = "\\d+"; //0-9 one or more times
                     //Makes sure the textfields not are empty
                     if (!txtfldConsumption.getText().trim().isEmpty() &&
                             !txtfldGadgetName.getText().trim().isEmpty()) {
 
-                        if (txtfldConsumption.getText().trim().matches(regex)) {
+                        if (txtfldConsumption.getText().trim().matches(reg)) {
                             String room = dropMenuRooms.getSelectionModel().getSelectedItem();
                             String gadget = dropMenuGadgets.getSelectionModel().getSelectedItem();
                             String nameOfGadget = txtfldGadgetName.getText();
@@ -208,15 +253,24 @@ public class GadgetsController implements DynamicFrame {
 
                             //Creating Laas string according to protocol.
                             String serverRequest = String.format("%s%s%s%s%s%s%s%s", "6:", gadget, ":", nameOfGadget, ":", room, ":", consumption);
-
-                            /*try {
+                            System.out.println(serverRequest);
+                            try {
                                 //putting serverRequest into requestToServer list.
                                 Main.getMainWindowController().requestsToServer.put(serverRequest);
+
+                            //setting all buttons,textfields and dropmenus as disabled so user have to click for example "add gadget"
+                            //after the request has been sent and none exceptions has been caught
+                            txtfldConsumption.setDisable(true);
+                            txtfldGadgetName.setDisable(true);
+                            dropMenuGadgets.setDisable(true);
+                            dropMenuRooms.setDisable(true);
+                            btnSubmit.setDisable(true);
+
                             } catch (InterruptedException ie) {
                                 ie.printStackTrace();
                             } catch (Exception e) {
                                 Main.getMainWindowController().exceptionLabel.setText("Could not add gadget..");
-                            }*/
+                            }
                         } else {
                             Main.getMainWindowController().exceptionLabel.setText("Please enter a valid consumption number..");
                         }
@@ -227,16 +281,8 @@ public class GadgetsController implements DynamicFrame {
                     Main.getMainWindowController().exceptionLabel.setText("Room and gadget needs to be selected for insertion..");
                 }
                 break;
-
             default:
                 Main.getMainWindowController().exceptionLabel.setText("Could not preform Action requested..");
         }
-
-        //setting all buttons,textfields and dropmenus as disabled so user have to click for example "add gadget"
-        txtfldConsumption.setDisable(true);
-        txtfldGadgetName.setDisable(true);
-        dropMenuGadgets.setDisable(true);
-        dropMenuRooms.setDisable(true);
-        btnSubmit.setDisable(true);
     }
 }
