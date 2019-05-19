af7c24de967bf3587adc54e565569289090a33f6
Sebastian <sebastian.martensson95@gmail.com>
2019-05-19 22:54:09 +0200

    adding available gadgets to blueprint

--- ClientApplication/src/mainPackage/houseFrame/BlueprintController.java
+++ ClientApplication/src/mainPackage/houseFrame/BlueprintController.java
@@ -1,26 +1,67 @@
 package mainPackage.houseFrame;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 import mainPackage.DynamicFrame;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import mainPackage.Main;
+import mainPackage.modelClasses.Gadget;
+import mainPackage.modelClasses.GuiObject;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.ArrayList;
 
-public class BlueprintController implements DynamicFrame, Initializable {
+public class BlueprintController implements DynamicFrame {
 
     @FXML
     private AnchorPane blueprint;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    private TableView<GuiObject> tblVKitchen;
+    @FXML
+    private TableColumn<GuiObject, ImageView> clmKitchen;
+
+    @FXML
+    private TableView<GuiObject> tblVLivingRoom;
+    @FXML
+    private TableColumn<GuiObject, ImageView> clmLivingroom;
+
+    @FXML
+    private TableView<GuiObject> tblVToilet;
+    @FXML
+    private TableColumn<GuiObject, ImageView> clmToilet;
+
+    @FXML
+    private TableView<GuiObject> tblVBedRoom;
+    @FXML
+    private TableColumn<GuiObject, ImageView> clmBedroom;
+
+    @FXML
+    private TableView<GuiObject> tblVGarage;
+    @FXML
+    private TableColumn<GuiObject, ImageView> clmGarage;
+
+
+    ArrayList<GuiObject> kitchenGadgetList = new ArrayList<>();
+    ArrayList<GuiObject> livingroomGadgetList = new ArrayList<>();
+    ArrayList<GuiObject> toiletGadgetList = new ArrayList<>();
+    ArrayList<GuiObject> bedroomGadgetList = new ArrayList<>();
+    ArrayList<GuiObject> garageGadgetList = new ArrayList<>();
+
+    public void initialize() {
+        clmKitchen.setCellValueFactory(new PropertyValueFactory<>("typeOfGadget"));
+        clmLivingroom.setCellValueFactory(new PropertyValueFactory<>("typeOfGadget"));
+        clmToilet.setCellValueFactory(new PropertyValueFactory<>("typeOfGadget"));
+        clmGarage.setCellValueFactory(new PropertyValueFactory<>("typeOfGadget"));
+        clmBedroom.setCellValueFactory(new PropertyValueFactory<>("typeOfGadget"));
+
         for (Node node : blueprint.getChildren()) {
             if (node instanceof Label) {
                 node.setVisible(false);
@@ -40,15 +81,13 @@ public class BlueprintController implements DynamicFrame, Initializable {
                             }
                         }
                 );
-
             }
         });
-
-
     }
 
     @Override
     public void updateFrame() {
+        showGadgetOnBlueprint();
     }
 
     @FXML
@@ -66,6 +105,54 @@ public class BlueprintController implements DynamicFrame, Initializable {
                     node.setStyle("-fx-text-fill: #1e97d2");
                 }
             }
+        }
+    }
+
+    public void showGadgetOnBlueprint() {
+        String typeOfGadget;
+        try {
+            for (Gadget g : Main.getMainWindowController().gadgetList) {
+                switch (g.getRoom()) {
+                    case "Kitchen":
+                        typeOfGadget = g.getClass().getSimpleName() + g.getState();
+                        GuiObject kitchenGuiObject = new GuiObject(typeOfGadget);
+                        kitchenGadgetList.add(kitchenGuiObject);
+                        break;
+                    case "Livingroom":
+                        typeOfGadget = g.getClass().getSimpleName() + g.getState();
+                        GuiObject livingroomGuiObject = new GuiObject(typeOfGadget);
+                        livingroomGadgetList.add(livingroomGuiObject);
+                        break;
+                    case "Toilet":
+                        typeOfGadget = g.getClass().getSimpleName() + g.getState();
+                        GuiObject toiletGuiObject = new GuiObject(typeOfGadget);
+                        toiletGadgetList.add(toiletGuiObject);
+                        break;
+                    case "Garage":
+                        typeOfGadget = g.getClass().getSimpleName() + g.getState();
+                        GuiObject garageGuiObject = new GuiObject(typeOfGadget);
+                        garageGadgetList.add(garageGuiObject);
+                        break;
+                    case "Bedroom":
+                        typeOfGadget = g.getClass().getSimpleName() + g.getState();
+                        GuiObject bedroomGuiObject = new GuiObject(typeOfGadget);
+                        bedroomGadgetList.add(bedroomGuiObject);
+                        break;
+                }
+            }
+            tblVKitchen.getItems().clear();
+            tblVKitchen.getItems().addAll(kitchenGadgetList);
+            tblVLivingRoom.getItems().clear();
+            tblVLivingRoom.getItems().addAll(livingroomGadgetList);
+            tblVToilet.getItems().clear();
+            tblVToilet.getItems().addAll(toiletGadgetList);
+            tblVGarage.getItems().clear();
+            tblVGarage.getItems().addAll(garageGadgetList);
+            tblVBedRoom.getItems().clear();
+            tblVBedRoom.getItems().addAll(bedroomGadgetList);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Main.getMainWindowController().exceptionLabel.setText("Could not load gadgets into blueprint");
         }
     }
 }
